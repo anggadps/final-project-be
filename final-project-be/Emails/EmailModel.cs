8b25de70bb1d462a30aec1d72d8ecd75dc1adea1
Angga Dharma <anggadharmaps@gmail.com>
2023-09-14 21:00:23 +0700

    add user activation and forgot pass

---
+++ final-project-be/Emails/EmailModel.cs
@@ -0,0 +1,28 @@
+﻿namespace final_project_be.Emails
+{
+    public class EmailModel
+    {
+        // Receiver
+        public List<string> To { get; }
+        public List<string> Cc { get; }
+        public List<string> Bcc { get; }
+
+        // Content
+        public string Subject { get; }
+        public string Body { get; }
+
+        public EmailModel(List<string> to,
+                          string subject,
+                          string? body = null,
+                          List<string> cc = null,
+                          List<string> bcc = null)
+        {
+            To = to;
+            Cc = cc ?? new List<string>();
+            Bcc = bcc ?? new List<string>();
+
+            Subject = subject;
+            Body = body;
+        }
+    }
+}
---
+++ final-project-be/Emails/Template/ActivationModel.cs
@@ -0,0 +1,9 @@
+﻿namespace final_project_be.Emails.Template
+{
+    public class ActivationModel
+    {
+        public string? Email { get; set; }
+        public string? Link { get; set; }
+
+    }
+}
