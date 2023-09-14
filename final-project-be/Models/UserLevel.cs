8ef08b17f4a01554ec7a33efafe5c8be425c4bfc
Muhamad Rizky Akbar Siddiq <rizkyakbar.rpl22020@smkn4bogor.sch.id>
2023-09-14 10:31:11 +0000

    Revert "Merge branch 'unfinished-email-activation' into 'main'"
    This reverts merge request !12

--- final-project-be/Models/UserLevel.cs
+++ final-project-be/Models/UserLevel.cs
@@ -2,7 +2,7 @@ namespace final_project_be.Models
 {
     public class UserLevel
     {
-        public Guid Id { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
     }
 }
