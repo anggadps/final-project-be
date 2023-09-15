a1645f8684f01f6bc3e95c0b19f5391b055c90d7
Angga Dharma <anggadharmaps@gmail.com>
2023-09-15 18:07:43 +0700

    Revert "fix -conflict"
    This reverts commit 69b5c0652f76aa7778db12533fdc405293338c74.

--- final-project-be/Controllers/LoginRequestDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace final_project_be.Controllers
-{
-    public class LoginRequestDTO
-    {
-        public string? Email { get; internal set; }
-        public string? Password { get; internal set; }
-    }
-}
