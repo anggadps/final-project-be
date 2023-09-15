69b5c0652f76aa7778db12533fdc405293338c74
Angga Dharma <anggadharmaps@gmail.com>
2023-09-15 18:06:38 +0700

    fix -conflict

---
+++ final-project-be/Controllers/LoginRequestDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace final_project_be.Controllers
+{
+    public class LoginRequestDTO
+    {
+        public string? Email { get; internal set; }
+        public string? Password { get; internal set; }
+    }
+}
---
+++ final-project-be/Controllers/ResetPasswordDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace final_project_be.Controllers
+{
+    public class ResetPasswordDTO
+    {
+        public string Password { get; set; } = string.Empty;
+        public string ConfirmPassword { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+
+    }
+}
