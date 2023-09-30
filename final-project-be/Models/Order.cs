4e6077cd4dcc72ef04e8e404db2f25305c479fec
Angga Dharma <anggadharmaps@gmail.com>
2023-09-30 19:18:14 +0700

    update OrderDataAccess (ViewInvoiceDetail)

--- final-project-be/Models/Order.cs
+++ final-project-be/Models/Order.cs
@@ -5,6 +5,10 @@ namespace final_project_be.Models
         public Guid Id { get; set; }
         public string No_invoice { get; set; } = string.Empty;
         public string? Id_user { get; set; }
+        public string? Course_name { get; set; }
+        public string? Category_name { get; set; }
+        public DateTime Schedule_date  { get; set; }
+        public decimal Price { get; set; }
         public decimal Total_course { get; set; }
         public decimal Total_price { get; set; }
         public DateTime Pay_date { get; set;}
